281c3275d72508887a3ae9141bdd71c400ca1ea9
SeryogaGitHub <main.email.seryoga@gmail.com>
2019-11-11 21:47:47 +0200

    Updates

---
+++ precache-manifest.36446d3b61b91c2bd421b4f6b9138a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3637a400e687a16bed7ca8bf53ae8cef",
+    "url": "/project-react/index.html"
+  },
+  {
+    "revision": "201effe2d65d89b9c932",
+    "url": "/project-react/static/css/main.4184deb2.chunk.css"
+  },
+  {
+    "revision": "a49c5b5dd30e54afeb52",
+    "url": "/project-react/static/js/2.1a250e5e.chunk.js"
+  },
+  {
+    "revision": "336d75d8cbb0777704b3",
+    "url": "/project-react/static/js/3.cf97c1a0.chunk.js"
+  },
+  {
+    "revision": "201effe2d65d89b9c932",
+    "url": "/project-react/static/js/main.b7dcade2.chunk.js"
+  },
+  {
+    "revision": "124ba57a8f434237fca5",
+    "url": "/project-react/static/js/runtime~main.2f328978.js"
+  },
+  {
+    "revision": "1d7626e6cbbdc02be15b16fa36add9ae",
+    "url": "/project-react/static/media/preloader.1d7626e6.gif"
+  }
+]);
